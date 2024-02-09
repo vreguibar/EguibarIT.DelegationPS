78ceb8078a21bb80d064c4190569109a07c96a06
vreguibar <Vicente@EguibarIT.com>
2024-02-09 14:23:04 +0100

    regex pattern has been slightly modified to ensure it matches a broader range of valid DNs, including those with multiple OU and DC components

--- Private/Test-IsValidDN.ps1
+++ Private/Test-IsValidDN.ps1
@@ -1,30 +1,36 @@
 function Test-IsValidDN {
     <#
         .SYNOPSIS
-            Cmdlet will check if the input string is a valid distinguishedname.
+            Validates if the input string is a valid distinguished name (DN).
 
         .DESCRIPTION
-            Cmdlet will check if the input string is a valid distinguishedname.
+            This cmdlet checks if the provided input string adheres to the structure of a valid distinguished name in Active Directory.
 
-            Cmdlet is intended as a dignostic tool for input validation
+            It is designed as a diagnostic tool to facilitate input validation for scripts and functions that manipulate Active Directory objects.
 
         .PARAMETER ObjectDN
-            A string representing the object distinguishedname.
+            The distinguished name to validate. This parameter accepts a string representing the DN of an Active Directory object.
 
         .EXAMPLE
-            Test-IsValidDN -ObjectDN 'Value1'
+            Test-IsValidDN -ObjectDN 'CN=Darth Vader,OU=Users,DC=EguibarIT,DC=local'
+
+            Returns $true if the input string is a valid DN, $false otherwise.
+
 
         .NOTES
             https://pscustomobject.github.io/powershell/howto/identity%20management/PowerShell-Check-If-String-Is-A-DN/
-            Version:         1.0
-            DateModified:    08/Oct/2021
+
+            Version:         1.1
+            DateModified:    09/Feb/2024
             LasModifiedBy:   Vicente Rodriguez Eguibar
                 vicente@eguibar.com
                 Eguibar Information Technology S.L.
                 http://www.eguibarit.com
     #>
+
     [CmdletBinding(ConfirmImpact = 'Low', SupportsShouldProcess = $true)]
     [OutputType([bool])]
+
     param
     (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
@@ -35,29 +41,52 @@ function Test-IsValidDN {
         [string]
         $ObjectDN
     )
+
     Begin {
+        Write-Verbose -Message '|=> ************************************************************************ <=|'
+        Write-Verbose -Message (Get-Date).ToShortDateString()
+        Write-Verbose -Message ('  Starting: {0}' -f $MyInvocation.Mycommand)
+        Write-Verbose -Message ('Parameters used by the function... {0}' -f (Set-FunctionDisplay $PsBoundParameters -Verbose:$False))
+
+        $isValid = $false
+
         # Define DN Regex
-        [regex]$distinguishedNameRegex = '^(?:(?<cn>CN=(?<name>(?:[^,]|\,)*)),)?(?:(?<path>(?:(?:CN|OU)=(?:[^,]|\,)+,?)+),)?(?<domain>(?:DC=(?:[^,]|\,)+,?)+)$'
+        #[regex]$distinguishedNameRegex = '^(?:(?<cn>CN=(?<name>(?:[^,]|\,)*)),)?(?:(?<path>(?:(?:CN|OU)=(?:[^,]|\,)+,?)+),)?(?<domain>(?:DC=(?:[^,]|\,)+,?)+)$'
+        [regex]$distinguishedNameRegex = '^(?:(?<cn>CN=(?<name>(?:[^,]+|\,)+)),)?(?:(?<ou>OU=(?:[^,]+|\,)+,?)*)(?<dc>DC=(?:[^,]+|\,)+)(?:,DC=(?:[^,]+|\,)+)*$'
+
+        Write-Verbose 'Begin block: Regex pattern for DN validation initialized.'
+
     } #end Begin
+
     Process {
+
         Try {
             # Use ShouldProcess to confirm the operation
             if ($PSCmdlet.ShouldProcess($ObjectDN, 'Validate DistinguishedName')) {
                 # Perform the actual validation
-                $isValid = $ObjectDN -match $distinguishedNameRegex
+                #$isValid = $ObjectDN -match $distinguishedNameRegex
+                $isValid = $distinguishedNameRegex.IsMatch($ObjectDN)
 
                 # Provide verbose output
                 if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                    Write-Verbose "DistinguishedName validation result: $isValid"
+                    Write-Verbose -Message ('DistinguishedName validation result: {0}' -f $isValid)
                 } #end If
+
             } #end If
-        }
-        catch {
+        } catch {
             # Handle exceptions gracefully
             Get-CurrentErrorToDisplay -CurrentError $error[0]
         } #end Try-Catch
+
     } #end Process
+
     end {
+
+        Write-Verbose -Message "Function $($MyInvocation.InvocationName) finished."
+        Write-Verbose -Message ''
+        Write-Verbose -Message '-------------------------------------------------------------------------------'
+        Write-Verbose -Message ''
+
         return $isValid
     } #end End
 } #end Function
