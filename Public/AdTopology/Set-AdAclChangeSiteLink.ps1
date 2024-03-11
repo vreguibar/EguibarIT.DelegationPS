3e02a888347eb47bf265802b817c74e3d1c73d94
vreguibar <Vicente@EguibarIT.com>
2024-03-11 20:19:26 +0100

    Void return type, shouldprocess and verbose output

--- Public/AdTopology/Set-AdAclChangeSiteLink.ps1
+++ Public/AdTopology/Set-AdAclChangeSiteLink.ps1
@@ -19,14 +19,15 @@
                 Set-AclConstructor5                    | EguibarIT.Delegation
                 New-GuidObjectHashTable                | EguibarIT.Delegation
         .NOTES
-            Version:         1.1
-            DateModified:    17/Oct/2016
+            Version:         1.2
+            DateModified:    11/Mar/2024
             LasModifiedBy:   Vicente Rodriguez Eguibar
                 vicente@eguibar.com
                 Eguibar Information Technology S.L.
                 http://www.eguibarit.com
     #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+    [OutputType([void])]
 
     param (
         # PARAM1 STRING for the Delegated Group Name
@@ -34,7 +35,7 @@
             HelpMessage = 'Group Name which will get the delegation',
             Position = 0)]
         [ValidateNotNullOrEmpty()]
-        [Alias('IdentityReference','Identity','Trustee','GroupID')]
+        [Alias('IdentityReference', 'Identity', 'Trustee', 'GroupID')]
         [String]
         $Group,
 
@@ -55,19 +56,22 @@
 
         ##############################
         # Variables Definition
-        $parameters = $null
+        [Hashtable]$Splat = [hashtable]::New()
 
         If ( ($null -eq $Variables.GuidMap) -and
-                 ($Variables.GuidMap -ne 0)     -and
-                 ($Variables.GuidMap -ne '')    -and
+                 ($Variables.GuidMap -ne 0) -and
+                 ($Variables.GuidMap -ne '') -and
                  (   ($Variables.GuidMap -isnot [array]) -or
                      ($Variables.GuidMap.Length -ne 0)) -and
                  ($Variables.GuidMap -ne $false)
-            ) {
+        ) {
+
             # $Variables.GuidMap is empty. Call function to fill it up
             Write-Verbose -Message 'Variable $Variables.GuidMap is empty. Calling function to fill it up.'
-            New-GuidObjectHashTable
-        }
+            Get-AttributeSchemaHashTable
+
+        } #end If
+
     } #end Begin
 
     process {
@@ -81,7 +85,7 @@
             InheritedObjectType   : All [GuidNULL]
             IsInherited           : False
         #>
-        $parameters = @{
+        $Splat = @{
             Id                    = $PSBoundParameters['Group']
             LDAPPath              = 'CN=Sites,{0}' -f $Variables.configurationNamingContext.ToString()
             AdRight               = 'ReadProperty', 'WriteProperty'
@@ -90,14 +94,27 @@
             AdSecurityInheritance = 'All'
         }
         # Check if RemoveRule switch is present.
-        If($PSBoundParameters['RemoveRule']) {
-            # Add the parameter to remove the rule
-            $parameters.Add('RemoveRule', $true)
+        If ($PSBoundParameters['RemoveRule']) {
+
+            if ($PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions to Change AD Site Link')) {
+                # Add the parameter to remove the rule
+                $Splat.Add('RemoveRule', $true)
+            } #end If
         }
-        Set-AclConstructor5 @parameters
+
+        If ($PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions to Change AD Site Link?')) {
+            Set-AclConstructor5 @Splat
+        } #end If
     }
 
     end {
+
+        if ($RemoveRule) {
+            Write-Verbose ('Permissions removal process completed for group: {0}' -f $PSBoundParameters['Group'])
+        } else {
+            Write-Verbose ('Permissions delegation process completed for group: {0}' -f $PSBoundParameters['Group'])
+        } #end If-Else
+
         Write-Verbose -Message "Function $($MyInvocation.InvocationName) finished."
         Write-Verbose -Message ''
         Write-Verbose -Message '-------------------------------------------------------------------------------'
