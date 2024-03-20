90de891eb6bfe7f364d786813af4aa7a66394d4a
vreguibar <Vicente@EguibarIT.com>
2024-03-20 14:18:24 +0100

    Delete duplicated nested function

--- Public/ServiceAccounts/Set-AdAclCreateDeleteGMSA.ps1
+++ Public/ServiceAccounts/Set-AdAclCreateDeleteGMSA.ps1
@@ -1,6 +1,5 @@
-﻿Function Set-AdAclCreateDeleteGMSA {
-    Function Set-AdAclCreateDeleteMSA {
-        <#
+﻿Function Set-AdAclCreateDeleteMSA {
+    <#
             .Synopsis
                 The function will delegate the premission for a group to Create/Delete Group Managed Service Accounts
             .DESCRIPTION
@@ -30,62 +29,62 @@
                     Eguibar Information Technology S.L.
                     http://www.eguibarit.com
         #>
-        [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
-        [OutputType([void])]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+    [OutputType([void])]
 
-        Param (
-            # PARAM1 STRING for the Delegated Group Name
-            [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
-                HelpMessage = 'Identity of the group getting the delegation, usually a DomainLocal group.',
-                Position = 0)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('IdentityReference', 'Identity', 'Trustee', 'GroupID')]
-            [String]
-            $Group,
+    Param (
+        # PARAM1 STRING for the Delegated Group Name
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Identity of the group getting the delegation, usually a DomainLocal group.',
+            Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('IdentityReference', 'Identity', 'Trustee', 'GroupID')]
+        [String]
+        $Group,
 
-            # PARAM2 Distinguished Name of the OU where the computer will get password reset
-            [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
-                HelpMessage = 'Distinguished Name of the OU where the computer will get password reset',
-                Position = 1)]
-            [ValidateNotNullOrEmpty()]
-            [String]
-            $LDAPpath,
+        # PARAM2 Distinguished Name of the OU where the computer will get password reset
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Distinguished Name of the OU where the computer will get password reset',
+            Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $LDAPpath,
 
-            # PARAM3 SWITCH If present, the access rule will be removed.
-            [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
-                HelpMessage = 'If present, the access rule will be removed.',
-                Position = 2)]
-            [ValidateNotNullOrEmpty()]
-            [Switch]
-            $RemoveRule
-        )
+        # PARAM3 SWITCH If present, the access rule will be removed.
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'If present, the access rule will be removed.',
+            Position = 2)]
+        [ValidateNotNullOrEmpty()]
+        [Switch]
+        $RemoveRule
+    )
 
-        Begin {
-            Write-Verbose -Message '|=> ************************************************************************ <=|'
-            Write-Verbose -Message (Get-Date).ToShortDateString()
-            Write-Verbose -Message ('  Starting: {0}' -f $MyInvocation.Mycommand)
-            Write-Verbose -Message ('Parameters used by the function... {0}' -f (Get-FunctionDisplay $PsBoundParameters -Verbose:$False))
+    Begin {
+        Write-Verbose -Message '|=> ************************************************************************ <=|'
+        Write-Verbose -Message (Get-Date).ToShortDateString()
+        Write-Verbose -Message ('  Starting: {0}' -f $MyInvocation.Mycommand)
+        Write-Verbose -Message ('Parameters used by the function... {0}' -f (Get-FunctionDisplay $PsBoundParameters -Verbose:$False))
 
-            ##############################
-            # Variables Definition
-            [Hashtable]$Splat = [hashtable]::New()
+        ##############################
+        # Variables Definition
+        [Hashtable]$Splat = [hashtable]::New()
 
-            If ( ($null -eq $Variables.GuidMap) -and
+        If ( ($null -eq $Variables.GuidMap) -and
                      ($Variables.GuidMap -ne 0) -and
                      ($Variables.GuidMap -ne '') -and
                      (   ($Variables.GuidMap -isnot [array]) -or
                          ($Variables.GuidMap.Length -ne 0)) -and
                      ($Variables.GuidMap -ne $false)
-            ) {
-                # $Variables.GuidMap is empty. Call function to fill it up
-                Write-Verbose -Message 'Variable $Variables.GuidMap is empty. Calling function to fill it up.'
-                Get-AttributeSchemaHashTable
-            } #end If
+        ) {
+            # $Variables.GuidMap is empty. Call function to fill it up
+            Write-Verbose -Message 'Variable $Variables.GuidMap is empty. Calling function to fill it up.'
+            Get-AttributeSchemaHashTable
+        } #end If
 
-        } #end Begin
+    } #end Begin
 
-        Process {
-            <#
+    Process {
+        <#
                 ACE number: 1
             --------------------------------------------------------
                   IdentityReference : XXX
@@ -96,28 +95,28 @@
                 InheritedObjectType : GuidNULL
                         IsInherited = False
             #>
-            $Splat = @{
-                Id                    = $PSBoundParameters['Group']
-                LDAPPath              = $PSBoundParameters['LDAPpath']
-                AdRight               = 'ListChildren', 'ReadProperty', 'Delete', 'GenericWrite', 'WriteDacl'
-                AccessControlType     = 'Allow'
-                ObjectType            = $Constants.GuidNULL
-                AdSecurityInheritance = 'All'
-            }
-            # Check if RemoveRule switch is present.
-            If ($PSBoundParameters['RemoveRule']) {
+        $Splat = @{
+            Id                    = $PSBoundParameters['Group']
+            LDAPPath              = $PSBoundParameters['LDAPpath']
+            AdRight               = 'ListChildren', 'ReadProperty', 'Delete', 'GenericWrite', 'WriteDacl'
+            AccessControlType     = 'Allow'
+            ObjectType            = $Constants.GuidNULL
+            AdSecurityInheritance = 'All'
+        }
+        # Check if RemoveRule switch is present.
+        If ($PSBoundParameters['RemoveRule']) {
 
-                if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for GroupManagedServiceAccount?')) {
-                    # Add the parameter to remove the rule
-                    $Splat.Add('RemoveRule', $true)
-                } #end If
+            if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for GroupManagedServiceAccount?')) {
+                # Add the parameter to remove the rule
+                $Splat.Add('RemoveRule', $true)
             } #end If
+        } #end If
 
-            If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for GroupManagedServiceAccount?')) {
-                Set-AclConstructor5 @Splat
-            } #end If
+        If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for GroupManagedServiceAccount?')) {
+            Set-AclConstructor5 @Splat
+        } #end If
 
-            <#
+        <#
                 ACE number: 2
             --------------------------------------------------------
                   IdentityReference : XXX
@@ -128,28 +127,28 @@
                 InheritedObjectType : GuidNULL
                         IsInherited = False
             #>
-            $Splat = @{
-                Id                    = $PSBoundParameters['Group']
-                LDAPPath              = $PSBoundParameters['LDAPpath']
-                AdRight               = 'CreateChild', 'DeleteChild'
-                AccessControlType     = 'Allow'
-                ObjectType            = $Variables.GuidMap['msDS-GroupManagedServiceAccount']
-                AdSecurityInheritance = 'All'
-            }
-            # Check if RemoveRule switch is present.
-            If ($PSBoundParameters['RemoveRule']) {
+        $Splat = @{
+            Id                    = $PSBoundParameters['Group']
+            LDAPPath              = $PSBoundParameters['LDAPpath']
+            AdRight               = 'CreateChild', 'DeleteChild'
+            AccessControlType     = 'Allow'
+            ObjectType            = $Variables.GuidMap['msDS-GroupManagedServiceAccount']
+            AdSecurityInheritance = 'All'
+        }
+        # Check if RemoveRule switch is present.
+        If ($PSBoundParameters['RemoveRule']) {
 
-                if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for msDS-GroupManagedServiceAccount?')) {
-                    # Add the parameter to remove the rule
-                    $Splat.Add('RemoveRule', $true)
-                } #end If
+            if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for msDS-GroupManagedServiceAccount?')) {
+                # Add the parameter to remove the rule
+                $Splat.Add('RemoveRule', $true)
             } #end If
+        } #end If
 
-            If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for msDS-GroupManagedServiceAccount?')) {
-                Set-AclConstructor5 @Splat
-            } #end If
+        If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for msDS-GroupManagedServiceAccount?')) {
+            Set-AclConstructor5 @Splat
+        } #end If
 
-            <#
+        <#
                 ACE number: 3
             --------------------------------------------------------
                   IdentityReference : XXX
@@ -160,28 +159,28 @@
                 InheritedObjectType : GuidNULL
                         IsInherited = False
             #>
-            $Splat = @{
-                Id                    = $PSBoundParameters['Group']
-                LDAPPath              = $PSBoundParameters['LDAPpath']
-                AdRight               = 'ReadProperty', 'WriteProperty'
-                AccessControlType     = 'Allow'
-                ObjectType            = $Variables.GuidMap['msDS-GroupManagedServiceAccount']
-                AdSecurityInheritance = 'All'
-            }
-            # Check if RemoveRule switch is present.
-            If ($PSBoundParameters['RemoveRule']) {
+        $Splat = @{
+            Id                    = $PSBoundParameters['Group']
+            LDAPPath              = $PSBoundParameters['LDAPpath']
+            AdRight               = 'ReadProperty', 'WriteProperty'
+            AccessControlType     = 'Allow'
+            ObjectType            = $Variables.GuidMap['msDS-GroupManagedServiceAccount']
+            AdSecurityInheritance = 'All'
+        }
+        # Check if RemoveRule switch is present.
+        If ($PSBoundParameters['RemoveRule']) {
 
-                if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for msDS-GroupManagedServiceAccount?')) {
-                    # Add the parameter to remove the rule
-                    $Splat.Add('RemoveRule', $true)
-                } #end If
+            if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for msDS-GroupManagedServiceAccount?')) {
+                # Add the parameter to remove the rule
+                $Splat.Add('RemoveRule', $true)
             } #end If
+        } #end If
 
-            If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for msDS-GroupManagedServiceAccount?')) {
-                Set-AclConstructor5 @Splat
-            } #end If
+        If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for msDS-GroupManagedServiceAccount?')) {
+            Set-AclConstructor5 @Splat
+        } #end If
 
-            <#
+        <#
                 ACE number: 4
                 --------------------------------------------------------
                       IdentityReference : XXX
@@ -192,42 +191,42 @@
                     InheritedObjectType : msDS-ManagedServiceAccount [ClassSchema]
                             IsInherited = False
             #>
-            $Splat = @{
-                Id                    = $PSBoundParameters['Group']
-                LDAPPath              = $PSBoundParameters['LDAPpath']
-                AdRight               = 'CreateChild', 'DeleteChild'
-                AccessControlType     = 'Allow'
-                ObjectType            = $Variables.GuidMap['applicationVersion']
-                AdSecurityInheritance = 'Descendents'
-                InheritedObjectType   = $Variables.GuidMap['msDS-GroupManagedServiceAccount']
-            }
-            # Check if RemoveRule switch is present.
-            If ($PSBoundParameters['RemoveRule']) {
+        $Splat = @{
+            Id                    = $PSBoundParameters['Group']
+            LDAPPath              = $PSBoundParameters['LDAPpath']
+            AdRight               = 'CreateChild', 'DeleteChild'
+            AccessControlType     = 'Allow'
+            ObjectType            = $Variables.GuidMap['applicationVersion']
+            AdSecurityInheritance = 'Descendents'
+            InheritedObjectType   = $Variables.GuidMap['msDS-GroupManagedServiceAccount']
+        }
+        # Check if RemoveRule switch is present.
+        If ($PSBoundParameters['RemoveRule']) {
 
-                if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for applicationVersion?')) {
-                    # Add the parameter to remove the rule
-                    $Splat.Add('RemoveRule', $true)
-                } #end If
+            if ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Remove permissions for applicationVersion?')) {
+                # Add the parameter to remove the rule
+                $Splat.Add('RemoveRule', $true)
             } #end If
+        } #end If
 
-            If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for applicationVersion?')) {
-                Set-AclConstructor6 @Splat
-            } #end If
-        } #end Process
+        If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Delegate the permisssions for applicationVersion?')) {
+            Set-AclConstructor6 @Splat
+        } #end If
+    } #end Process
 
-        End {
+    End {
 
-            if ($RemoveRule) {
-                Write-Verbose ('Permissions removal process completed for group: {0} on {1}' -f $PSBoundParameters['Group'], $PSBoundParameters['LDAPpath'])
-            } else {
-                Write-Verbose ('Permissions delegation process completed for group: {0} on {1}' -f $PSBoundParameters['Group'], $PSBoundParameters['LDAPpath'])
-            } #end If-Else
+        if ($RemoveRule) {
+            Write-Verbose ('Permissions removal process completed for group: {0} on {1}' -f $PSBoundParameters['Group'], $PSBoundParameters['LDAPpath'])
+        } else {
+            Write-Verbose ('Permissions delegation process completed for group: {0} on {1}' -f $PSBoundParameters['Group'], $PSBoundParameters['LDAPpath'])
+        } #end If-Else
 
-            Write-Verbose -Message "Function $($MyInvocation.InvocationName) Create Delete MSA."
-            Write-Verbose -Message ''
-            Write-Verbose -Message '--------------------------------------------------------------------------------'
-            Write-Verbose -Message ''
-        } #end END
-    }
-
+        Write-Verbose -Message "Function $($MyInvocation.InvocationName) Create Delete MSA."
+        Write-Verbose -Message ''
+        Write-Verbose -Message '--------------------------------------------------------------------------------'
+        Write-Verbose -Message ''
+    } #end END
 }
+
+
