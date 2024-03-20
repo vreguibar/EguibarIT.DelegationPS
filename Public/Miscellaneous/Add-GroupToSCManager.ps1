c41542f3af7e35aaa2236811015c9ca8f9b7a84b
vreguibar <Vicente@EguibarIT.com>
2024-03-20 13:46:59 +0100

    Permit to view Services in DomainControllers without Admin privileges

---
+++ Public/Miscellaneous/Add-GroupToSCManager.ps1
@@ -0,0 +1,109 @@
+﻿Function Add-GroupToSCManager {
+    <#
+        .Synopsis
+            Adds a group to the Service Control Manager (SCM) ACL.
+
+        .DESCRIPTION
+            This function adds a specified group to the Service Control Manager (SCM) ACL with specified permissions.
+
+        .EXAMPLE
+            Add-GroupToSCManager -Group "EguibarIT\SG_AdAdmins"
+
+        .EXAMPLE
+            $Splat = @{
+                Group = "EguibarIT\SG_AdAdmins"
+                Verbose = $true
+            }
+            Add-GroupToSCManager @Splat
+
+        .PARAMETER Group
+            Specifies the group to be added to the SCM ACL.
+
+        .NOTES
+            This function relies on SC.exe located at $env:SystemRoot\System32\
+
+        .NOTES
+            Version:         1.0
+            DateModified:    20/Mar/2024
+            LasModifiedBy:   Vicente Rodriguez Eguibar
+                vicente@eguibar.com
+                Eguibar IT
+                http://www.eguibarit.com
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+    [OutputType([void])]
+
+    Param (
+        # PARAM1 STRING for the Delegated Group Name
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Identity of the group getting the delegation, usually a DomainLocal group.',
+            Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('IdentityReference', 'Identity', 'Trustee', 'GroupID')]
+        [String]
+        $Group
+    )
+
+    Begin {
+
+        $error.clear()
+
+        Write-Verbose -Message '|=> ************************************************************************ <=|'
+        Write-Verbose -Message (Get-Date).ToShortDateString()
+        Write-Verbose -Message ('  Starting: {0}' -f $MyInvocation.Mycommand)
+        Write-Verbose -Message ('Parameters used by the function... {0}' -f (Set-FunctionDisplay $PsBoundParameters -Verbose:$False))
+
+        ##############################
+        # Variables Definition
+
+        $MySDDL = $null
+
+        # New ACL variable
+        $NewAcl = [System.Security.AccessControl.DirectorySecurity]::New()
+
+        # Define new Access Rule using above indicated Account
+        $Rule = [System.Security.AccessControl.FileSystemAccessRule]::New($PSBoundParameters['Group'], 'ReadData, AppendData, ReadPermissions', 'None', 'None', 'Allow')
+
+
+    } #end Begin
+
+    Process {
+
+        # get current 'Service Control Manager (SCM)' acl in SDDL format
+        Write-Verbose -Message 'get current "Service Control Manager (SCM)" acl in SDDL format'
+        $MySDDL = ((& (Get-Command "$($env:SystemRoot)\System32\sc.exe") @('sdshow', 'scmanager'))[1])
+
+        Write-Verbose -Message 'Original SDDL...'
+        $MySDDL
+
+        # Import SDDL into variable
+        Write-Verbose -Message 'Import "Service Control Manager (SCM)" SDDL into NewAcl variable'
+        $NewAcl.SetSecurityDescriptorSddlForm($MySDDL)
+
+        # Add new Access Rule to ACL variable
+        Write-Verbose -Message 'Add new Access Rule into NewAcl variable'
+        $NewAcl.SetAccessRule($Rule)
+
+        Write-Verbose -Message 'New Access will be...'
+        $NewAcl.Access
+
+        Write-Verbose -Message 'Updated SDDL will be...'
+        $NewAcl.Sddl
+
+        If ($Force -or $PSCmdlet.ShouldProcess($PSBoundParameters['Group'], 'Update permissions on SCM?')) {
+
+            Write-Verbose -Message 'Updating SCM Access'
+            $null = (& (Get-Command "$($env:SystemRoot)\System32\sc.exe") @('sdset', 'scmanager', "$($NewAcl.Sddl)"))
+
+        } #end If
+
+
+    } #end Process
+
+    End {
+        Write-Verbose -Message "Function $($MyInvocation.InvocationName) finished changing Service Control Manager (SCM) access."
+        Write-Verbose -Message ''
+        Write-Verbose -Message '-------------------------------------------------------------------------------'
+        Write-Verbose -Message ''
+    } #end END
+} # End Function
