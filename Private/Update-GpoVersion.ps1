9a4d86b7cbe3967f779d1275dcdb928f68ca8468
vreguibar <vicente@EguibarIT.com>
2025-03-20 18:34:08 +0100

    Updated version with Test

--- Private/Update-GpoVersion.ps1
+++ Private/Update-GpoVersion.ps1
@@ -3,35 +3,81 @@ function Update-GpoVersion {
 
     <#
         .SYNOPSIS
-            Updates the version number of a specified Group Policy Object (GPO).
+            Updates the version number of specified Group Policy Objects (GPOs).
 
         .DESCRIPTION
-            The Update-GpoVersion function increments the computer version number of a specified GPO by 3. It updates both the directory object and the GPT.INI file in the SYSVOL share.
+            The Update-GpoVersion function increments the computer version number of specified GPOs by 3.
+            It updates both the directory object and the GPT.INI file in the SYSVOL share.
+            Supports both single GPO updates and batch processing through pipeline input.
 
         .PARAMETER GpoName
-            The name of the GPO to be updated.
+            The name(s) of the GPO(s) to be updated. Accepts pipeline input.
+
+        .PARAMETER IncrementBy
+            The number to increment the version by. Defaults to 3.
 
         .EXAMPLE
             Update-GpoVersion -GpoName "Default Domain Policy"
+            Updates the specified GPO version number.
+
+        .EXAMPLE
+            Get-GPO -All | Where-Object {$_.DisplayName -like "*Security*"} | Update-GpoVersion
+            Updates version numbers for all GPOs with "Security" in their name.
 
         .INPUTS
-            GPO Name.
+            System.String
+            Microsoft.GroupPolicy.Gpo
 
         .OUTPUTS
-            None.
+            None
+
+        .NOTES
+            Used Functions:
+                Name                       ║ Module/Namespace
+                ═══════════════════════════╬══════════════════════════════
+                Get-GPO                    ║ GroupPolicy
+                Write-Verbose              ║ Microsoft.PowerShell.Utility
+                Get-FunctionDisplay        ║ EguibarIT
+                Import-MyModule            ║ EguibarIT
+
+        .NOTES
+            Version:         1.1
+            DateModified:   20/Mar/2025
+            LastModifiedBy: Vicente Rodriguez Eguibar
+                        vicente@eguibar.com
+                        Eguibar IT
+                        http://www.eguibarit.com
+
+
+
+        .LINK
+            https://github.com/vreguibar/EguibarIT.DelegationPS/blob/main/Private/Update-GpoVersion.ps1
     #>
 
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+    [CmdletBinding(SupportsShouldProcess = $true,
+        ConfirmImpact = 'Medium')]
     [OutputType([void])]
 
     param (
+
         [Parameter(Mandatory = $true,
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
             HelpMessage = 'Specify the name of the GPO.',
             Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Name', 'DisplayName')]
         [string]
-        $GpoName
+        $GpoName,
+
+        [Parameter(Mandatory = $false,
+            HelpMessage = 'Specify the increment number.',
+            Position = 2)]
+        [ValidateRange(1, 100)]
+        [PSDefaultValue(Help = 'Default Value is "3"')]
+        [int]
+        $IncrementBy = 3
+
     )
 
     Begin {
@@ -41,9 +87,9 @@ function Update-GpoVersion {
         # Display function header if variables exist
         if ($null -ne $Variables -and $null -ne $Variables.HeaderDelegation) {
             $txt = ($Variables.HeaderDelegation -f
-            (Get-Date).ToString('dd/MMM/yyyy'),
+                (Get-Date).ToString('dd/MMM/yyyy'),
                 $MyInvocation.Mycommand,
-            (Get-FunctionDisplay -HashTable $PsBoundParameters -Verbose:$False)
+                (Get-FunctionDisplay -HashTable $PsBoundParameters -Verbose:$False)
             )
             Write-Verbose -Message $txt
         } #end if
@@ -58,11 +104,15 @@ function Update-GpoVersion {
 
         # Retrieve the GPO object by name
         $gpo = Get-GPO -Name $PsBoundParameters['GpoName'] -ErrorAction Stop
+
         # Get the GPO ID
         $gpoId = ('{' + $gpo.Id + '}')
+
         # Build SYSVOL path
         $sysVolPath = '\\{0}\SYSVOL\{0}' -f $env:USERDNSDOMAIN
         $pathToGpt = '{0}\Policies\{1}\gpt.ini' -f $sysVolPath, $gpoId
+
+        Write-Debug -Message ('Path to GPT: {0}' -f $pathToGpt)
 
     } #end Begin
 
@@ -74,16 +124,16 @@ function Update-GpoVersion {
             $url = 'LDAP://CN={0},CN=Policies,CN=System,{1}' -f $gpoId, $Variables.defaultNamingContext
             $de = [System.DirectoryServices.DirectoryEntry]::New($url)
 
+            Write-Debug -Message ('Accessing GPO through DirectoryEntry: {0}' -f $url)
+
         } catch {
 
             Write-Error -Message ('Error accessing GPO through DirectoryEntry' -f $Gpo.Name)
-            #Get-ErrorDetail -ErrorRecord $_
 
         } #end Try-Catch
 
         # Get the VersionObject of the DirectoryEntry (the GPO)
         $versionObject = [Int64]($de.Properties['VersionNumber'].Value.ToString())
-        Write-Verbose -Message ('Old GPO Version Number: {0}' -f $versionObject)
 
         # Convert the value into a 8 digit HEX string
         $hexValue = $versionObject.ToString('x8')
@@ -99,8 +149,8 @@ function Update-GpoVersion {
         # Lower 16 bits as Integer ComputerVersionNumber
         $computerVN = [Convert]::ToInt64($hexComputerVN, 16)
 
-        # Increment Computer Version Number by 3
-        $computerVN += 3
+        # Use IncrementBy parameter
+        $computerVN += $IncrementBy
 
         # Concatenate '0x' and 'HEX UserVersionNumber having 4 digits' and 'HEX ComputerVersionNumber having 4 digits'
         $newHex = '0x{0}{1}' -f $hexUserVN, $computerVN.ToString('x4')
@@ -112,6 +162,7 @@ function Update-GpoVersion {
         try {
 
             if ($PSCmdlet.ShouldProcess($GpoName, 'Update GPO version')) {
+
                 # Update the GPO VersionNumber with the new value
                 $de.Properties['VersionNumber'].Value = $newVersionObject.ToString()
 
@@ -126,48 +177,69 @@ function Update-GpoVersion {
                 # Close the DirectoryEntry
                 $de.Close()
 
-                # Write new version value to GPT (Including Section Name)
+                Write-Debug -Message ('Old GPO Version Number: {0}' -f $versionObject)
+                Write-Debug -Message ('New GPO Version Number: {0}' -f $newVersionObject)
+
+                # Write new version value to GPT (Including Section Name). Update SYSVOL file
                 if (Test-Path -Path $pathToGpt) {
 
-                    # New instance of IniFile class
-                    $Gpt = [IniFileHandler.IniFile]::new($pathToGpt)
-                    #$Gpt.ReadFile($pathToGpt)
+                    try {
+                        # New instance of IniFile class
+                        $Gpt = [IniFileHandler.IniFile]::new($pathToGpt)
 
-                    # Check section exists
-                    if ($Gpt.SectionExists('General')) {
-                        Write-Verbose -Message ('Section Name: General')
+                        # Check section exists
+                        if ($Gpt.SectionExists('General')) {
 
-                        # Change value of an existing key
-                        $Gpt.SetKeyValue('General', 'Version', $newVersionObject.ToString())
-                        $Gpt.SetKeyValue('General', 'displayName', $Gpo.DisplayName)
+                            Write-Debug -Message ('Section Name: General')
 
-                    } else {
-                        Write-Verbose -Message 'Section [General] does not exist. Creating it with Key=Value.'
+                            # Change value of an existing key
+                            $Gpt.SetKeyValue('General', 'Version', $newVersionObject.ToString())
+                            $Gpt.SetKeyValue('General', 'displayName', $Gpo.DisplayName)
 
-                        $Gpt.AddSection('General')
+                        } else {
 
-                        # Add a new Key/Value pair within a given section
-                        $Gpt.SetKeyValue('General', 'Version', $newVersionObject.ToString())
-                        $Gpt.SetKeyValue('General', 'displayName', $Gpo.DisplayName)
-                    } #end If-Else
+                            Write-Debug -Message 'Section [General] does not exist. Creating it with Key=Value.'
 
-                    # Save file using default encoding UTF-8
-                    $Gpt.SaveFile($pathToGpt)
+                            $Gpt.AddSection('General')
 
-                    Write-Verbose -Message ('Saving new Version of GPO to file {0}' -f $pathToGpt)
+                            # Add a new Key/Value pair within a given section
+                            $Gpt.SetKeyValue('General', 'Version', $newVersionObject.ToString())
+                            $Gpt.SetKeyValue('General', 'displayName', $Gpo.DisplayName)
+
+                        } #end If-Else
+
+                        # Save file using default encoding UTF-8
+                        $Gpt.SaveFile($pathToGpt)
+
+                        Write-Debug -Message ('Saving new Version of GPO {0} to file {1}' -f $Gpo.DisplayName, $pathToGpt)
+
+                    } catch {
+
+                        Write-Error -Message ('Failed to update GPT.INI for {0}: {1}' -f $Gpo.DisplayName, $_.Exception.Message)
+                        continue
+
+                    } #end Try-Catch
+
                 } #end If
+
             } #end If
+
         } catch {
-            #Get-ErrorDetail -ErrorRecord $_
+
             throw "The GPTs.ini file could not be modified: $_. Message is $($_.Exception.Message)"
+
         } #end Try-Catch
 
     } #end Process
 
     End {
-        $txt = ($Variables.FooterDelegation -f $MyInvocation.InvocationName,
-            'Version of GPO updated (Private Function).'
-        )
-        Write-Verbose -Message $txt
+
+        if ($null -ne $Variables -and $null -ne $Variables.FooterDelegation) {
+            $txt = ($Variables.FooterDelegation -f $MyInvocation.InvocationName,
+                'Version of GPO updated (Private Function).'
+            )
+            Write-Verbose -Message $txt
+        } #end if
+
     } #end End
-}
+} #end function Update-GpoVersion
---
+++ Tests/Update-GpoVersion.Test.ps1
@@ -0,0 +1,172 @@
+﻿BeforeAll {
+    # Import required modules
+    Import-Module -Name 'GroupPolicy' -Force
+    Import-Module -Name 'EguibarIT' -Force
+    Import-Module -Name 'EguibarIT.DelegationPS' -Force
+
+    # Mock Variables
+    $script:mockGpoName = 'Test GPO'
+    $script:mockGuid = 'E47B9889-3A1A-4A7B-9C7E-1234567890AB'
+    $script:mockDomain = 'contoso.com'
+    $script:mockGptPath = "\\$mockDomain\SYSVOL\$mockDomain\Policies\{$mockGuid}\gpt.ini"
+    $script:mockLdapPath = "LDAP://CN={$mockGuid},CN=Policies,CN=System,DC=contoso,DC=com"
+
+    # Mock the environment variable
+    $env:USERDNSDOMAIN = $mockDomain
+
+    # Mock helper functions
+    function Test-AdminPrivilege {
+        return $true
+    }
+    function Import-MyModule {
+    }
+    function Get-FunctionDisplay {
+        return 'Test Display'
+    }
+
+    # Mock GPO object
+    $script:mockGpo = @{
+        DisplayName = $mockGpoName
+        Id          = $mockGuid
+    }
+
+    # Mock DirectoryEntry
+    $script:mockDe = @{
+        Properties    = @{
+            'VersionNumber'            = @{ Value = '65536' }  # Hex: 0x00010000
+            'gPCMachineExtensionNames' = @{ Value = '' }
+        }
+        CommitChanges = { }
+        Close         = { }
+    }
+
+    # Mock IniFile handler
+    $script:mockIniFile = @{
+        SectionExists = { param($section) return $false }
+        AddSection    = { param($section) }
+        SetKeyValue   = { param($section, $key, $value) }
+        SaveFile      = { param($path) }
+    }
+}
+
+Describe 'Update-GpoVersion' {
+    BeforeEach {
+        # Mock core cmdlets
+        Mock -CommandName Get-GPO -MockWith { $mockGpo }
+        Mock -CommandName Test-Path -MockWith { $true }
+        Mock -CommandName Write-Error
+        Mock -CommandName Write-Debug
+        Mock -CommandName Write-Verbose
+        Mock -CommandName Write-Progress
+
+        # Mock .NET classes
+        Mock -CommandName New-Object -ParameterFilter {
+            $TypeName -eq 'System.DirectoryServices.DirectoryEntry'
+        } -MockWith { $mockDe }
+    }
+
+    Context 'Parameter Validation' {
+        It 'Should have mandatory GpoName parameter' {
+            (Get-Command Update-GpoVersion).Parameters['GpoName'].Attributes.Mandatory |
+                Should -BeTrue
+        }
+
+        It 'Should accept pipeline input' {
+            (Get-Command Update-GpoVersion).Parameters['GpoName'].Attributes.ValueFromPipeline |
+                Should -BeTrue
+        }
+
+        It 'Should validate IncrementBy range' {
+            { Update-GpoVersion -GpoName $mockGpoName -IncrementBy 0 } |
+                Should -Throw
+            { Update-GpoVersion -GpoName $mockGpoName -IncrementBy 101 } |
+                Should -Throw
+        }
+    }
+
+    Context 'GPO Version Calculations' {
+        It 'Should correctly increment version number' {
+            # Arrange
+            $initialVersion = '65536'  # Hex: 0x00010000
+            $expectedNewVersion = '65539'  # Hex: 0x00010003 (increment by 3)
+            $mockDe.Properties['VersionNumber'].Value = $initialVersion
+
+            # Act
+            Update-GpoVersion -GpoName $mockGpoName -WhatIf:$false
+
+            # Assert
+            Should -Invoke -CommandName Write-Debug -ParameterFilter {
+                $Message -match "New GPO Version Number: $expectedNewVersion"
+            }
+        }
+
+        It 'Should handle custom increment value' {
+            # Arrange
+            $initialVersion = '65536'  # Hex: 0x00010000
+            $expectedNewVersion = '65541'  # Hex: 0x00010005 (increment by 5)
+            $mockDe.Properties['VersionNumber'].Value = $initialVersion
+
+            # Act
+            Update-GpoVersion -GpoName $mockGpoName -IncrementBy 5 -WhatIf:$false
+
+            # Assert
+            Should -Invoke -CommandName Write-Debug -ParameterFilter {
+                $Message -match "New GPO Version Number: $expectedNewVersion"
+            }
+        }
+    }
+
+    Context 'Error Handling' {
+        It 'Should handle non-existent GPO' {
+            # Arrange
+            Mock -CommandName Get-GPO -MockWith { throw [Microsoft.GroupPolicy.GPNotFoundException]::new() }
+
+            # Act
+            Update-GpoVersion -GpoName 'NonExistentGPO'
+
+            # Assert
+            Should -Invoke -CommandName Write-Error -ParameterFilter {
+                $Message -match 'GPO not found'
+            }
+        }
+
+        It 'Should handle inaccessible SYSVOL path' {
+            # Arrange
+            Mock -CommandName Test-Path -MockWith { $false }
+
+            # Act
+            Update-GpoVersion -GpoName $mockGpoName
+
+            # Assert
+            Should -Invoke -CommandName Write-Error -ParameterFilter {
+                $Message -match 'GPT.INI not found'
+            }
+        }
+    }
+
+    Context 'Pipeline Input' {
+        It 'Should process multiple GPOs from pipeline' {
+            # Arrange
+            $gpos = @($mockGpoName, 'Second GPO', 'Third GPO')
+
+            # Act
+            $gpos | Update-GpoVersion -WhatIf:$false
+
+            # Assert
+            Should -Invoke -CommandName Get-GPO -Times 3
+            Should -Invoke -CommandName Write-Progress -Times 3
+        }
+    }
+
+    Context 'WhatIf Support' {
+        It 'Should not modify GPO when using -WhatIf' {
+            # Act
+            Update-GpoVersion -GpoName $mockGpoName -WhatIf
+
+            # Assert
+            Should -Invoke -CommandName Get-GPO -Times 1
+            Should -Not -Invoke Write-Error
+            $mockDe.Properties['VersionNumber'].Value | Should -Be '65536'
+        }
+    }
+}
