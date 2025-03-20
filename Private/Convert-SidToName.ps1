c22f76dc7d345995732a1e11815b414e04b94ba3
vreguibar <vicente@EguibarIT.com>
2025-03-20 10:16:44 +0100

    Bump

--- Private/Convert-SidToName.ps1
+++ Private/Convert-SidToName.ps1
@@ -93,9 +93,9 @@
         # Display function header if variables exist
         if ($null -ne $Variables -and $null -ne $Variables.HeaderDelegation) {
             $txt = ($Variables.HeaderDelegation -f
-            (Get-Date).ToString('dd/MMM/yyyy'),
+                (Get-Date).ToString('dd/MMM/yyyy'),
                 $MyInvocation.Mycommand,
-            (Get-FunctionDisplay -HashTable $PsBoundParameters -Verbose:$False)
+                (Get-FunctionDisplay -HashTable $PsBoundParameters -Verbose:$False)
             )
             Write-Verbose -Message $txt
         } #end if
